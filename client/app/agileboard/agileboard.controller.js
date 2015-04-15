27d219f6e51c1237312755f1ac81ccf32403b962
Anuj Verma <anujverma000@gmail.com>
2015-04-14 23:03:46 -0700

    Add and list all agile boards

--- client/app/agileboard/agileboard.controller.js
+++ client/app/agileboard/agileboard.controller.js
@@ -2,5 +2,17 @@
 
 angular.module('agileRetroApp')
   .controller('AgileboardCtrl', function ($scope, $http, Auth, User) {
+  		$scope.agileboards = [];
+  		$http.get('/api/agileboards').success(function(agileboards) {
+	      $scope.agileboards = agileboards;
+	      socket.syncUpdates('agileboards', $scope.agileboards);
+	    });
 
+	    $scope.addAgileBoard = function() {
+	      if($scope.name === '') {
+	        return;
+	      }
+	      $http.post('/api/agileboards', { name: $scope.name, info: $scope.info, active: true });
+	      $scope.newThing = '';
+	    };
   });
